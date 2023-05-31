a729d683712a37a923ed74032b6aa082f61c1253
jshuckbot <gorka_ivan@mail.ru>
2023-05-31 14:44:55 +0700

    less_1 task_3 added func guess_number

---
+++ lesson_1/task_3.py
@@ -0,0 +1,23 @@
+from random import randint
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+ATTEMPTS = 10
+
+MSG_GUESS = 'Вы угадали число'
+MSG_LESS = 'Вы ввели число меньше загадонного'
+MSG_MORE = 'Вы ввели число больше загадонного'
+MSG_LOSE = 'Вы проиграли! кончились попытки'
+MSG_INPUT_NUMBER = 'Введите загаданное число: '
+
+
+def guess_number(happy_number, number) -> tuple:
+    """Угадывает число"""
+    if number == happy_number:
+        return True, MSG_GUESS
+    elif number > happy_number:
+        return False, MSG_MORE
+    elif number < happy_number:
+        return False, MSG_LESS
+
+
